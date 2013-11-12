19adf8b64fdd69bfc459ed5217c686cf8461d041
Breschie <d_breschie@arcor.de>
2013-11-12 11:27:02 +0100

    first commit

---
+++ Kernel/Language/de_AgentTicketQuickClose.pm
@@ -0,0 +1,26 @@
+﻿# --
+# Kernel/Language/de_AgentTicketQuickClose.pm - translation file
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_AgentTicketQuickClose;
+
+use strict;
+use warnings;
+
+sub Data {
+    my $Self = shift;
+
+    $Self->{Translation}->{'Quick Close'} = 'erfolgreich Schließen';
+    $Self->{Translation}->{'Close this ticket'} = 'Dieses Ticket schließn';
+	$Self->{Translation}->{'Shows a link in the menu to quick close a ticket in the ticket zoom view of the agent interface.'} = 'Zeigt dem Agenten im Ticketmenü einen Link an, um Tickets schnell erfolgreich zu schließen.';
+	$Self->{Translation}->{'Defines the default close state for quick close.'} = 'Definiert den Standardwert-Status für die Funktion QuickClose.';
+	  
+    return 1;
+}
+1;
+
+
---
+++ README.md
@@ -0,0 +1,36 @@
+
+Denny Bresch - Quick Delete
+========================
+
+**Feature List**
+
+This package enables a "Quick Delete" feature in ticket action row (where you already have "note, pending, close, ..."). With it you can delete a ticket without having to leave a note.
+
+**Installation**
+
+Download the package and install it via admin interface -> package manager .
+
+
+**Prerequisites**
+
+- OTRS 3.1
+
+- OTRS 3.2
+
+**Configuration**
+
+Not needed.
+
+**Download**
+
+For download see [http://znuny.com/d/](http://znuny.com/d/)
+
+**Commercial Support**
+
+For this extension and for OTRS in general visit [http://znuny.com](http://znuny.com). Looking forward to hear from you!
+
+Enjoy!
+
+ Your Denny Bresch for Znuny!
+ [http://znuny.com](http://znuny.com)
+
