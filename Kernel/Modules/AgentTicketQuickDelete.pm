bddc399475040b43e3c8a8a13d5f6566b6975e1b
Gino Naumann <gino@nauminator.de>
2019-01-28 17:05:33 +0100

    preparation for OTRS 6

--- Kernel/Modules/AgentTicketQuickDelete.pm
+++ Kernel/Modules/AgentTicketQuickDelete.pm
@@ -1,6 +1,11 @@
-#
-# Copyright (C) 2013 Denny Bresch
-#
+# --
+# Copyright (C) (2013) (Denny Bresch) (dennybresch@gmail.com) (https://github.com/dennybresch)
+# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
 
 package Kernel::Modules::AgentTicketQuickDelete;
 
