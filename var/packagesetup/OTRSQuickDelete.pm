f784f31e90cc1d38675a14b30cba247eb2773a72
Gino Naumann <gino@nauminator.de>
2019-02-01 00:32:08 +0100

    add packagesetup script

---
+++ var/packagesetup/OTRSQuickDelete.pm
@@ -0,0 +1,182 @@
+# --
+# Copyright (C) 2019 Gino Naumann, https://github.com/SynPrime
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package var::packagesetup::OTRSQuickDelete;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::GenericAgent',
+    'Kernel::Config',
+    'Kernel::System::Queue',
+);
+
+=head1 NAME
+
+var::packagesetup::OTRSQuickDelete - code to execute during package installation
+
+=head1 DESCRIPTION
+
+Functions for installing the OTRSQuickDelete package.
+
+=head1 PUBLIC INTERFACE
+
+=head2 new()
+
+create an object
+
+    use Kernel::System::ObjectManager;
+    local $Kernel::OM = Kernel::System::ObjectManager->new();
+    my $CodeObject = $Kernel::OM->Get('var::packagesetup::OTRSQuickDelete');
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+=head2 CodeInstall()
+
+run the code install part
+
+    my $Result = $CodeObject->CodeInstall();
+
+=cut
+
+sub CodeInstall {
+    my ( $Self, %Param ) = @_;
+
+    my $GenericAgentObject  = $Kernel::OM->Get('Kernel::System::GenericAgent');
+    my $ConfigObject        = $Kernel::OM->Get('Kernel::Config');
+    my $QueueObject         = $Kernel::OM->Get('Kernel::System::Queue');
+
+    my $Config = $ConfigObject->Get('Ticket::Frontend::AgentTicketQuickDelete');
+
+    # my $Queue = $Config->{Queue};
+    my $Queue   = "Junk";
+    my $QueueID = $QueueObject->QueueLookup( Queue => $Queue );
+
+    my %JobList = $GenericAgentObject->JobList();
+
+    my $JobExists;
+
+    for my $GenericAgent ( sort keys %JobList ) {
+        my %Job = $GenericAgentObject->JobGet( Name => $GenericAgent );
+        if (
+            $Job{NewDelete} == 1
+            &&
+            ( exists($Job{Queue})
+               && $Job{Queue} eq "$Queue"
+               || grep { $_ eq "$QueueID" } @{$Job{QueueIDs}}
+            )
+           )
+        {
+            $JobExists = 1;
+        }
+    }
+
+    if ($QueueID && ! $JobExists == 1) {
+        $GenericAgentObject->JobAdd(
+            Name => 'QuickDelete',
+            UserID => 1,
+            ValidID => 1,
+            Data => {
+                Valid => 1,
+                ScheduleDays => [
+                    '0',
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6'
+                ],
+                ScheduleMinutes => [ '0' ],
+                ScheduleHours => [ '1' ],
+                QueueIDs => [
+                    "$QueueID"
+                ],
+                NewDelete => '1',
+            },
+        );
+    }
+
+    return 1;
+}
+
+=head2 CodeUpgrade()
+
+run the code upgrade part
+
+    my $Result = $CodeObject->CodeUpgrade();
+
+=cut
+
+sub CodeUpgrade {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=head2 CodeReinstall()
+
+run the code reinstall part
+
+    my $Result = $CodeObject->CodeReinstall();
+
+=cut
+
+sub CodeReinstall {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=head2 CodeUninstall()
+
+run the code uninstall part
+
+    my $Result = $CodeObject->CodeUninstall();
+
+=cut
+
+sub CodeUninstall {
+    my ( $Self, %Param ) = @_;
+
+    my $GenericAgentObject  = $Kernel::OM->Get('Kernel::System::GenericAgent');
+
+    my $JobName = "QuickDelete";
+    my %Job     = $GenericAgentObject->JobGet(Name => "$JobName");
+
+    if (%Job) {
+        my $DeleteJob = $GenericAgentObject->JobDelete(
+            Name    => "$JobName",
+            UserID  => 1,
+        )
+    }
+
+    return 1;
+}
+
+1;
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
